eab44d5e37ef034a4a4623fc5fa09e96f48e14da
Florian Leroux <florian.leroux3@laposte.net>
2021-02-11 12:47:06 +0100

    init: mvcmusic app & todoapi

---
+++ MvcMusic/Models/MusicGenreViewModel.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMusic.Models
+{
+    public class MusicGenreViewModel
+    {
+        public List<Music> Musics { get; set; }
+        public SelectList Genres { get; set; }
+        public string MusicGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
---
+++ MvcMusic/Models/SeedData.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMusic.Data;
+using System;
+using System.Linq;
+
+namespace MvcMusic.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMusicContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMusicContext>>()))
+            {
+                // Look for any Musics.
+                if (context.Music.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Music.AddRange(
+                    new Music
+                    {
+                        Title = "Random Access Memories",
+                        ReleaseDate = DateTime.Parse("2013-07-01"),
+                        Genre = "Dance",
+                        Price = 7.99M,
+                        Rating = "P",
+                    },
+
+                    new Music
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "Q",
+                    },
+
+                    new Music
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R",
+                    },
+
+                    new Music
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R",
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
